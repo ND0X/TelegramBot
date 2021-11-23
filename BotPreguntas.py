964f0b9b9296de9400c7763ed389d0ffed8aa10c
Diego Valdés <diego_alejo7@hotmail.com>
2021-11-22 23:37:03 -0300

    Programa Completo con Preguntas 2

--- BotPreguntas.py
+++ BotPreguntas.py
@@ -23,75 +23,75 @@ respuestas_correctas = 0
 random_question = Question('', '', '')
 lista_Preguntas = [Question('¿Cuál de las siguientes alternativas está asociado a lo que es un Repositorio de Código?',
                             ['Lugar donde se almacena y distribuye el código de una aplicación',
-                             'Suele contener diverso tipo de metadatos que acompañan al código'
+                             'Suele contener diverso tipo de metadatos que acompañan al código',
                              'Lugar para trabajar de manera asincrónica e individual'],
                             ['Lugar donde se almacena y distribuye el código de una aplicación',
                              'Suele contener diverso tipo de metadatos que acompañan al código']),
 
                    Question('¿Qué es un hotspot?',
                             ['Sección del código el cuál no es tan importante',
-                             'Sección de interés del código donde se requiere más esfuerzo'
+                             'Sección de interés del código donde se requiere más esfuerzo',
                              'Sección del código que trabajará una sola persona'],
                             ['Sección de interés del código donde se requiere más esfuerzo']),
 
                    Question('¿Cuál de las siguientes sentencias describe un repositorio Git?',
                             ['Base de datos donde se almacena los archivos de un proyecto',
-                             'Provee una copia de todos los archivos y una copia del repositorio en sí'
+                             'Provee una copia de todos los archivos y una copia del repositorio en sí',
                              'Base de datos con todo lo necesario para manejar la historia de un proyecto'],
                             ['Provee una copia de todos los archivos y una copia del repositorio en sí',
                              'Base de datos con todo lo necesario para manejar la historia de un proyecto']),
 
                    Question('¿Cuál de las siguientes alternativas corresponden a estructuras de un repositorio Git?',
                             ['Blobs, trees, commits, tags, index',
-                             'Archivos, carpetas, ficheros .exe'
+                             'Archivos, carpetas, ficheros .exe',
                              'Bits, SHA-1'],
                             ['Blobs, trees, commits, tags, index']),
 
                    Question('¿Cuál de las siguientes alternativas describe un beneficio para usar una rama (tree)?',
                             ['Se puede usar para mostrar una versión del proyecton',
-                             'Siempre una rama representa el trabajo de una persona'
+                             'Siempre una rama representa el trabajo de una persona',
                              'Encapsula una fase de desarrollo (prototipo, alfa, beta, estable, etc)'],
                             ['Encapsula una fase de desarrollo (prototipo, alfa, beta, estable, etc)']),
 
                    Question('¿Cuándo se usa el algoritmo Fast-forward para fusionar ramas?',
                             ['Se utiliza cuando hay un desarrollo lineal entre las ramas que se quieren fusionar',
-                             'Se utiliza cuando no hay un desarrollo lineal entre las ramas'
+                             'Se utiliza cuando no hay un desarrollo lineal entre las ramas',
                              'Se utiliza en cualquier contexto'],
                             ['Se utiliza cuando hay un desarrollo lineal entre las ramas que se quieren fusionar']),
 
                    Question('¿Por qué modificar commits?',
                             ['Desagrupar un gran cambio en una serie de cambios pequeños',
-                             'Para que se vea estéticamente mejor el historial'
+                             'Para que se vea estéticamente mejor el historial',
                              'Para agregar código adicional al proyecto'],
                             ['Desagrupar un gran cambio en una serie de cambios pequeños']),
 
                    Question('¿Qué describe a un repositorio clon?',
                             ['Contiene todos los objetos del original, es independiente y permite el trabajo local',
-                             'Es una copia idéntica de otro repositorio'
+                             'Es una copia idéntica de otro repositorio',
                              'Es un repositorio el cual se actualiza automáticamente con el repositorio original creado'],
                             ['Contiene todos los objetos del original, es independiente y permite el trabajo local']),
 
                    Question('¿Qué beneficio nos otorga el seguimiento de tareas ?',
                             ['Permite visualizar más fácilmente los asuntos del proyecto',
-                             'Permiten organizar, filtrar y manejar eficientemente asuntos del proyecto (issues).'
+                             'Permiten organizar, filtrar y manejar eficientemente asuntos del proyecto (issues).',
                              'Permite llevar un conteo de los asuntos del proyecto'],
                             ['Permiten organizar, filtrar y manejar eficientemente asuntos del proyecto (issues).']),
 
                    Question('¿Qué utilidad tiene la herramienta Git hook?',
                             ['Herramienta de Git, que permite visualizar tareas',
-                             'Herramienta de Git, que permite el seguimiento de tareas'
+                             'Herramienta de Git, que permite el seguimiento de tareas',
                              'Herramienta de Git, que permite la automatización de tareas'],
                             ['Herramienta de Git, que permite la automatización de tareas']),
 
                    Question('¿Qué es la integración continua?',
-                            ['Permite que desarrolladores suban y fusionen cambios del código en una misma rama de forma frecuente',
-                             'Es una práctica para que los desarrolladores trabajen en una misma rama al mismo tiempo'
+                            ['Permite que desarrolladores suban/fusionen cambios del código en una misma rama de forma frecuente',
+                             'Es una práctica para que los desarrolladores trabajen en una misma rama al mismo tiempo',
                              'Permite que los desarrolladores trabajen de manera integra dentro de las carpetas del proyecto'],
-                            ['Permite que desarrolladores suban y fusionen cambios del código en una misma rama de forma frecuente']),
+                            ['Permite que desarrolladores suban/fusionen cambios del código en una misma rama de forma frecuente']),
 
                    Question('¿Qué son los GitHub Actions?',
                             ['Son herramientas para clonar y manejar repositorios',
-                             'Son herramientas para fusionar trabajar y fusionar ramas'
+                             'Son herramientas para fusionar trabajar y fusionar ramas',
                              'Son herramientas para automatizar, personalizar y ejecutar flujos en desarrollo de software'],
                             ['Son herramientas para automatizar, personalizar y ejecutar flujos en desarrollo de software']),
                    ]
